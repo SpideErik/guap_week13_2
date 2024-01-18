3341605e9bbb61ab98fe41ff953dd699bd80efc1
SpideErik <erikozh@gmail.com>
2024-01-18 18:36:49 +0300

    task1

--- task1.py
+++ task1.py
@@ -2,21 +2,14 @@ from random import randint
 from tkinter import *
 
 
-def plus():
+def change(d):
     global width, height
-    if width < 100:
-        width += 1
-    if height < 100:
-        height += 1
-    button.config(width=width, height=height, text=f'{width}x{height}')
-
-
-def minus():
-    global width, height
-    if width:
-        width -= 1
-    if height:
-        height -= 1
+    width += d
+    height += d
+    width = max(4, width)
+    width = min(100, width)
+    height = max(4, height)
+    height = min(100, height)
     button.config(width=width, height=height, text=f'{width}x{height}')
 
 
@@ -24,8 +17,8 @@ root = Tk()
 root.title('Button')
 width = 4
 height = 4
-button = Button(text=f'{width}x{height}', font='Arial 24', width=width, height=height, command=plus)
-button.bind('<Button-3>', lambda e: minus())
+button = Button(text=f'{width}x{height}', font='Arial 24', width=width, height=height, command=lambda: change(1))
+button.bind('<Button-3>', lambda e: change(-1))
 button.pack()
 
 root.mainloop()
